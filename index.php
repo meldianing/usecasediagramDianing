ddde45e476ab0626ba2ce80dcaed51ffa2f4d464
meldianing <35475106+meldianing@users.noreply.github.com>
2019-02-19 19:19:39 -0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
 
 <h2>dianing</h2>
 
-<img src="usecaseDianing.jpg"  width="100%" height="100%">
+<img src="1usecaseDianing.jpg"  width="100%" height="100%">
+  <img src="usecase.jpg"  width="100%" height="100%">
+
 
 </body>
 </html>
